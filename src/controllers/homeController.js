23c8b80164480449473e2957e1b36d7d1a1d273f
9ian9 <108910700+9ian9@users.noreply.github.com>
2024-05-06 01:01:45 +0700

    tao csdl video 24

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,16 @@
+import db from '../models/index';
+let getHomePage = async(req, res) => {
+    try {
+        let data = await db.User.findAll();
+        // console.log(data)
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+    } catch (e) {
+        console.log(e)
+    }
+
 }
 
 let getAboutPage = (req, res) => {
